59b3cbe84117e2d7b39fef7fd9310f4b20159cf1
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-06-20 17:02:45 +0200

    update throttler runner to be async

--- runner.go
+++ runner.go
@@ -2,42 +2,73 @@ package gohalt
 
 import (
 	"context"
+	"fmt"
+	"sync"
 )
 
 type Runnable func(context.Context) error
-type Logger func(string, ...interface{})
+type Logger func(...interface{})
 
-func discard(string, ...interface{}) {}
+func discard(...interface{}) {}
 
 type Runner struct {
-	t Throttler
-	l Logger
+	thr Throttler
+	ctx context.Context
+	wg  sync.WaitGroup
+
+	report func(error)
+	err    error
 }
 
-func NewRunner(t Throttler) Runner {
-	return Runner{t: t, l: discard}
+func NewRunner(ctx context.Context, thr Throttler) *Runner {
+	return NewRunnerWithLogger(ctx, thr, discard)
 }
 
-func NewRunnerWithLogger(t Throttler, l Logger) Runner {
-	return Runner{t: t, l: l}
-}
-
-func (r Runner) Go(ctx context.Context, run Runnable) (gerr error) {
-	if err := r.t.Acquire(ctx); err != nil {
-		r.l("throttler error happened: %w", err)
-		return err
+func NewRunnerWithLogger(ctx context.Context, thr Throttler, log Logger) *Runner {
+	ctx, cancel := context.WithCancel(ctx)
+	r := Runner{thr: thr, ctx: ctx}
+	var once sync.Once
+	r.report = func(err error) {
+		if err != nil {
+			log(err.Error())
+		}
+		once.Do(func() {
+			r.err = err
+			cancel()
+		})
 	}
-	defer func() {
-		if err := r.t.Release(ctx); err != nil {
-			r.l("throttler error happened: %w", err)
-			if gerr == nil {
-				gerr = err
+	return &r
+}
+
+func (r *Runner) Go(run Runnable) {
+	r.wg.Add(1)
+	go func() {
+		defer func() {
+			if err := r.thr.Release(r.ctx); err != nil {
+				r.report(fmt.Errorf("throttler error happened: %w", err))
 			}
+			r.wg.Done()
+			return
+		}()
+		if err := r.thr.Acquire(r.ctx); err != nil {
+			r.report(fmt.Errorf("throttler error happened: %w", err))
+			return
+		}
+		select {
+		case <-r.ctx.Done():
+			r.report(fmt.Errorf("context error happened: %w", r.ctx.Err()))
+			return
+		default:
+		}
+		if err := run(r.ctx); err != nil {
+			r.report(fmt.Errorf("run error happened: %w", err))
+			return
 		}
 	}()
-	if err := run(ctx); err != nil {
-		r.l("run error happened: %w", err)
-		return err
-	}
-	return nil
+}
+
+func (r *Runner) Wait() error {
+	r.wg.Wait()
+	r.report(nil)
+	return r.err
 }
