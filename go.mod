ade7b35a220634e2babe647142c5ea18059e8c5c
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-06-21 12:22:23 +0200

    add cpu stats to throttlers

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/1pkg/gohalt
 
 go 1.14
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/shirou/gopsutil v2.20.5+incompatible
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/sys v0.0.0-20200620081246-981b61492c35 // indirect
+)
--- stats.go
+++ stats.go
@@ -3,18 +3,20 @@ package gohalt
 import (
 	"runtime"
 	"time"
+
+	"github.com/shirou/gopsutil/cpu"
 )
 
 type Stats interface {
 	MEM() (alloc uint64, system uint64)
-	CPU() (avgpause uint64, usage float64)
+	CPU() (avgpause uint64, avgusage float64)
 }
 
 type cachedstats struct {
 	alloc    uint64
 	system   uint64
 	avgpause uint64
-	usage    float64
+	avgusage float64
 }
 
 func NewCachedStats(duration time.Duration) *cachedstats {
@@ -37,17 +39,23 @@ func (s cachedstats) MEM() (alloc uint64, system uint64) {
 	return s.alloc, s.system
 }
 
-func (s cachedstats) CPU() (avgpause uint64, usage float64) {
-	return s.avgpause, s.usage
+func (s cachedstats) CPU() (avgpause uint64, avgusage float64) {
+	return s.avgpause, s.avgusage
 }
 
 func (s *cachedstats) refresh() {
 	var stats runtime.MemStats
 	runtime.ReadMemStats(&stats)
+	s.alloc = stats.Alloc
+	s.system = stats.Sys
 	for _, p := range stats.PauseNs {
 		s.avgpause += p
 	}
 	s.avgpause /= 256
-	s.alloc = stats.Alloc
-	s.system = stats.Sys
+	if percents, err := cpu.Percent(10*time.Millisecond, true); err != nil {
+		for _, p := range percents {
+			s.avgusage += p
+		}
+		s.avgusage /= float64(len(percents))
+	}
 }
