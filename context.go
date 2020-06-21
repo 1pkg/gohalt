5468fa36a858f8c3845ccf2509210622c322499d
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-06-21 16:16:36 +0200

    add context key helper

--- context.go
+++ context.go
@@ -15,6 +15,10 @@ func WithKey(ctx context.Context, key interface{}) context.Context {
 	return context.WithValue(ctx, gohaltctxkey, key)
 }
 
+func ctxKey(ctx context.Context) interface{} {
+	return ctx.Value(gohaltctxkey)
+}
+
 func withTimestamp(ctx context.Context) context.Context {
 	return context.WithValue(ctx, gohaltctxtimestamp, time.Now().UTC().UnixNano())
 }
