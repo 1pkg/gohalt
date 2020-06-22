68d8f03a83fb867edbed36170d6aef68036e6286
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-06-22 22:25:34 +0200

    add kafka enqueuer implementation

--- enqueuer.go
+++ enqueuer.go
@@ -4,21 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"sync"
 	"time"
 
+	"github.com/segmentio/kafka-go"
 	"github.com/streadway/amqp"
 )
 
-var uuid uint64
-
 type Enqueuer interface {
 	Publish(context.Context, interface{}) error
-	Close() error
+	Close(context.Context) error
 }
+
+var exchanger uint64
 
 type amqpp struct {
 	conn *amqp.Connection
 	ch   *amqp.Channel
+	mut  sync.Mutex
 
 	pool time.Duration
 	url  string
@@ -27,17 +30,17 @@ type amqpp struct {
 }
 
 func NewPublisherJsonAmqp(ctx context.Context, url string, queue string, pool time.Duration) (*amqpp, error) {
-	uuid++
-	exchange := fmt.Sprintf("gohalt_exchange_%d", uuid)
+	exchanger++
+	exchange := fmt.Sprintf("gohalt_exchange_%d", exchanger)
 	enq := &amqpp{pool: pool, url: url, que: queue, exch: exchange}
-	if err := enq.connect(); err != nil {
+	if err := enq.connect(ctx); err != nil {
 		return nil, err
 	}
 	loop(ctx, pool, func(ctx context.Context) error {
-		if err := enq.Close(); err != nil {
+		if err := enq.Close(ctx); err != nil {
 			return err
 		}
-		enq.connect()
+		enq.connect(ctx)
 		return ctx.Err()
 	})
 	return enq, nil
@@ -48,6 +51,8 @@ func (enq *amqpp) Publish(ctx context.Context, data interface{}) error {
 	if err != nil {
 		return err
 	}
+	enq.mut.Lock()
+	defer enq.mut.Unlock()
 	return enq.ch.Publish(
 		enq.exch,
 		enq.que,
@@ -64,14 +69,16 @@ func (enq *amqpp) Publish(ctx context.Context, data interface{}) error {
 	)
 }
 
-func (enq *amqpp) Close() error {
+func (enq *amqpp) Close(context.Context) error {
+	enq.mut.Lock()
+	defer enq.mut.Unlock()
 	if err := enq.ch.Close(); err != nil {
 		return err
 	}
 	return enq.conn.Close()
 }
 
-func (enq *amqpp) connect() error {
+func (enq *amqpp) connect(context.Context) error {
 	conn, err := amqp.Dial(enq.url)
 	if err != nil {
 		return err
@@ -86,5 +93,68 @@ func (enq *amqpp) connect() error {
 	if err := ch.QueueBind(enq.que, enq.que, enq.exch, false, nil); err != nil {
 		return err
 	}
+	enq.mut.Lock()
+	enq.conn = conn
+	enq.ch = ch
+	enq.mut.Unlock()
+	return nil
+}
+
+type kafkap struct {
+	conn *kafka.Conn
+	mut  sync.Mutex
+
+	pool  time.Duration
+	net   string
+	url   string
+	topic string
+}
+
+func NewPublisherJsonKafka(ctx context.Context, network string, url string, topic string, pool time.Duration) (*kafkap, error) {
+	enq := &kafkap{net: network, url: url, topic: topic}
+	if err := enq.connect(ctx); err != nil {
+		return nil, err
+	}
+	loop(ctx, pool, func(ctx context.Context) error {
+		if err := enq.Close(ctx); err != nil {
+			return err
+		}
+		enq.connect(ctx)
+		return ctx.Err()
+	})
+	return enq, nil
+}
+
+func (enq *kafkap) Publish(ctx context.Context, data interface{}) error {
+	message, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	enq.mut.Lock()
+	defer enq.mut.Unlock()
+	if _, err := enq.conn.WriteMessages(kafka.Message{
+		Key:   []byte("gohalt_enqueue"),
+		Value: message,
+		Time:  time.Now().UTC(),
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (enq *kafkap) Close(ctx context.Context) error {
+	enq.mut.Lock()
+	defer enq.mut.Unlock()
+	return enq.conn.Close()
+}
+
+func (enq *kafkap) connect(ctx context.Context) error {
+	conn, err := kafka.DialLeader(ctx, enq.net, enq.url, enq.topic, 0)
+	if err != nil {
+		return err
+	}
+	enq.mut.Lock()
+	enq.conn = conn
+	enq.mut.Unlock()
 	return nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/segmentio/kafka-go v0.3.7
 	github.com/shirou/gopsutil v2.20.5+incompatible
 	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.6.1 // indirect
