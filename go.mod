8ef498dc8ea4f32ef9cc826d3b7a7b4cf8eff3fa
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-06-23 21:59:14 +0200

    add prometheus binary metric querier

--- go.mod
+++ go.mod
@@ -7,10 +7,8 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.7.0
+	github.com/prometheus/common v0.10.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/segmentio/kafka-go v0.3.7
 	github.com/shirou/gopsutil v2.20.5+incompatible
---
+++ metrics.go
@@ -0,0 +1,80 @@
+package gohalt
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/api"
+	prometheus "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+)
+
+type BinaryMetric interface {
+	Query(context.Context) (bool, error)
+}
+
+type mprometheus struct {
+	url   string
+	query string
+	exp   time.Duration
+	step  time.Duration
+
+	api prometheus.API
+	val bool
+	mut sync.Mutex
+}
+
+func NewBinaryMetricPrometheusCached(
+	ctx context.Context,
+	url string,
+	query string,
+	exp time.Duration,
+	step time.Duration,
+) (*mprometheus, error) {
+	client, err := api.NewClient(api.Config{
+		Address:      url,
+		RoundTripper: api.DefaultRoundTripper,
+	})
+	if err != nil {
+		return nil, err
+	}
+	m := mprometheus{
+		url:   url,
+		query: query,
+		exp:   exp,
+		step:  step,
+		api:   prometheus.NewAPI(client),
+	}
+	loop(ctx, exp, func(ctx context.Context) error {
+		if err := m.pull(ctx); err != nil {
+			return err
+		}
+		return ctx.Err()
+	})
+	return &m, m.pull(ctx)
+}
+
+func (m *mprometheus) Query(ctx context.Context) (bool, error) {
+	m.mut.Lock()
+	defer m.mut.Unlock()
+	return m.val, nil
+}
+
+func (m *mprometheus) pull(ctx context.Context) error {
+	timestamp := time.Now().UTC()
+	val, _, err := m.api.QueryRange(ctx, m.query, prometheus.Range{
+		Start: timestamp,
+		End:   timestamp.Add(m.exp),
+		Step:  m.step,
+	})
+	scalar, ok := val.(*model.Scalar)
+	if !ok || (scalar.Value != 0 && scalar.Value != 1) {
+		return fmt.Errorf("boolean metric value expected instead of %v", val)
+	}
+	m.mut.Lock()
+	m.val = scalar.Value == 1
+	m.mut.Unlock()
+	return err
+}
