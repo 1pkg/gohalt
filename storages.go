bc5ff8af011d16cdd7c17d7cfd4bf19b3de11dd2
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-07-01 23:37:15 +0200

    lazy storage connection

--- storages.go
+++ storages.go
@@ -1,16 +1,17 @@
 package gohalt
 
 import (
+	"context"
 	"errors"
 	"sync"
+	"time"
 
 	badger "github.com/dgraph-io/badger/v2"
 )
 
 type Storage interface {
-	Get() ([]byte, error)
-	Set([]byte) error
-	Close() error
+	Get(context.Context) ([]byte, error)
+	Set(context.Context, []byte) error
 }
 
 type smemory struct {
@@ -22,7 +23,7 @@ func NewStorageMemory() *smemory {
 	return &smemory{}
 }
 
-func (s *smemory) Get() ([]byte, error) {
+func (s *smemory) Get(context.Context) ([]byte, error) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	if s.buffer == nil {
@@ -31,7 +32,7 @@ func (s *smemory) Get() ([]byte, error) {
 	return s.buffer, nil
 }
 
-func (s *smemory) Set(data []byte) error {
+func (s *smemory) Set(ctx context.Context, data []byte) error {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	if data == nil {
@@ -41,27 +42,30 @@ func (s *smemory) Set(data []byte) error {
 	return nil
 }
 
-func (s *smemory) Close() error {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	s.buffer = nil
-	return nil
-}
-
 type sbadger struct {
-	db  *badger.DB
-	key []byte
+	memconnect Runnable
+	db         *badger.DB
+	key        []byte
 }
 
-func NewStorageBadger(path string, key string) (sbadger, error) {
-	db, err := badger.Open(badger.DefaultOptions(path))
-	return sbadger{
-		db:  db,
-		key: []byte(key),
-	}, err
+func NewStorageBadger(path string, key string, cache time.Duration) sbadger {
+	s := sbadger{key: []byte(key)}
+	var lock sync.Mutex
+	s.memconnect = cached(cache, func(ctx context.Context) error {
+		lock.Lock()
+		defer lock.Unlock()
+		if err := s.close(ctx); err != nil {
+			return err
+		}
+		return s.connect(ctx, path)
+	})
+	return s
 }
 
-func (s sbadger) Get() ([]byte, error) {
+func (s sbadger) Get(ctx context.Context) ([]byte, error) {
+	if err := s.memconnect(ctx); err != nil {
+		return nil, err
+	}
 	var result []byte
 	err := s.db.View(func(txn *badger.Txn) error {
 		item, err := txn.Get(s.key)
@@ -73,12 +77,24 @@ func (s sbadger) Get() ([]byte, error) {
 	return result, err
 }
 
-func (s sbadger) Set(data []byte) error {
+func (s sbadger) Set(ctx context.Context, data []byte) error {
+	if err := s.memconnect(ctx); err != nil {
+		return err
+	}
 	return s.db.Update(func(txn *badger.Txn) error {
 		return txn.Set(s.key, data)
 	})
 }
 
-func (s sbadger) Close() error {
+func (s sbadger) close(context.Context) error {
 	return s.db.Close()
+}
+
+func (s sbadger) connect(ctx context.Context, path string) error {
+	db, err := badger.Open(badger.DefaultOptions(path))
+	if err != nil {
+		return err
+	}
+	s.db = db
+	return nil
 }
