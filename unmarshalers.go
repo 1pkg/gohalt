5b6dce2e2f44b5f5ed862fff32699808090b076f
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-07-01 23:28:20 +0200

    fix unmarshaler signature

--- unmarshalers.go
+++ unmarshalers.go
@@ -18,94 +18,94 @@ func (um Unmarshaler) Unmarshal(ctx context.Context, bytes []byte, thr Throttler
 	if err := um(bytes, &umobj.obj); err != nil {
 		return err
 	}
-	thr.accept(ctx, &umobj)
+	thr.accept(ctx, umobj)
 	return nil
 }
 
-func (umobj *umobject) tvisitEcho(ctx context.Context, thr *techo) {
+func (umobj umobject) tvisitEcho(ctx context.Context, thr *techo) {
 }
 
-func (umobj *umobject) tvisitWait(ctx context.Context, thr *twait) {
+func (umobj umobject) tvisitWait(ctx context.Context, thr *twait) {
 }
 
-func (umobj *umobject) tvisitPanic(ctx context.Context, thr *tpanic) {
+func (umobj umobject) tvisitPanic(ctx context.Context, thr *tpanic) {
 }
 
-func (umobj *umobject) tvisitEach(ctx context.Context, thr *teach) {
+func (umobj umobject) tvisitEach(ctx context.Context, thr *teach) {
 	thr.current = uint64(umobj.obj.(float64))
 }
 
-func (umobj *umobject) tvisitAfter(ctx context.Context, thr *tafter) {
+func (umobj umobject) tvisitAfter(ctx context.Context, thr *tafter) {
 	thr.current = uint64(umobj.obj.(float64))
 }
 
-func (umobj *umobject) tvisitChance(ctx context.Context, thr *tchance) {
+func (umobj umobject) tvisitChance(ctx context.Context, thr *tchance) {
 }
 
-func (umobj *umobject) tvisitFixed(ctx context.Context, thr *tfixed) {
+func (umobj umobject) tvisitFixed(ctx context.Context, thr *tfixed) {
 	thr.current = uint64(umobj.obj.(float64))
 }
 
-func (umobj *umobject) tvisitRunning(ctx context.Context, thr *trunning) {
+func (umobj umobject) tvisitRunning(ctx context.Context, thr *trunning) {
 }
 
-func (umobj *umobject) tvisitBuffered(ctx context.Context, thr *tbuffered) {
+func (umobj umobject) tvisitBuffered(ctx context.Context, thr *tbuffered) {
 }
 
-func (umobj *umobject) tvisitPriority(ctx context.Context, thr *tpriority) {
+func (umobj umobject) tvisitPriority(ctx context.Context, thr *tpriority) {
 }
 
-func (umobj *umobject) tvisitTimed(ctx context.Context, thr *ttimed) {
+func (umobj umobject) tvisitTimed(ctx context.Context, thr *ttimed) {
 	thr.current = uint64(umobj.obj.(float64))
 }
 
-func (umobj *umobject) tvisitMonitor(ctx context.Context, thr *tmonitor) {
+func (umobj umobject) tvisitMonitor(ctx context.Context, thr *tmonitor) {
 }
 
-func (umobj *umobject) tvisitMetric(ctx context.Context, thr *tmetric) {
+func (umobj umobject) tvisitMetric(ctx context.Context, thr *tmetric) {
 }
 
-func (umobj *umobject) tvisitLatency(ctx context.Context, thr *tlatency) {
+func (umobj umobject) tvisitLatency(ctx context.Context, thr *tlatency) {
 }
 
-func (umobj *umobject) tvisitPercentile(ctx context.Context, thr *tpercentile) {
+func (umobj umobject) tvisitPercentile(ctx context.Context, thr *tpercentile) {
 }
 
-func (umobj *umobject) tvisitAdaptive(ctx context.Context, thr *tadaptive) {
+func (umobj umobject) tvisitAdaptive(ctx context.Context, thr *tadaptive) {
 	objs := umobj.obj.([]interface{})
 	thr.current = uint64(objs[0].([]interface{})[0].(float64))
 	thr.step = uint64(objs[0].([]interface{})[1].(float64))
 	tobj := umobject{obj: objs[1]}
-	thr.thr.accept(ctx, &tobj)
+	thr.thr.accept(ctx, tobj)
 }
 
-func (umobj *umobject) tvisitContext(ctx context.Context, thr *tcontext) {
+func (umobj umobject) tvisitContext(ctx context.Context, thr *tcontext) {
 }
 
-func (umobj *umobject) tvisitEnqueue(ctx context.Context, thr *tenqueue) {
+func (umobj umobject) tvisitEnqueue(ctx context.Context, thr *tenqueue) {
 }
 
-func (umobj *umobject) tvisitKeyed(ctx context.Context, thr *tkeyed) {
+func (umobj umobject) tvisitKeyed(ctx context.Context, thr *tkeyed) {
 }
 
-func (umobj *umobject) tvisitAll(ctx context.Context, thr *tall) {
+func (umobj umobject) tvisitAll(ctx context.Context, thr *tall) {
 	thrs := *thr
 	objs := umobj.obj.([]interface{})
 	for i := range thrs {
 		tobj := umobject{obj: objs[i]}
-		thrs[i].accept(ctx, &tobj)
+		thrs[i].accept(ctx, tobj)
 	}
 }
 
-func (umobj *umobject) tvisitAny(ctx context.Context, thr *tany) {
+func (umobj umobject) tvisitAny(ctx context.Context, thr *tany) {
 	thrs := *thr
 	objs := umobj.obj.([]interface{})
 	for i := range thrs {
 		tobj := umobject{obj: objs[i]}
-		thrs[i].accept(ctx, &tobj)
+		thrs[i].accept(ctx, tobj)
 	}
 }
 
-func (umobj *umobject) tvisitNot(ctx context.Context, thr *tnot) {
+func (umobj umobject) tvisitNot(ctx context.Context, thr *tnot) {
 	thr.thr.accept(ctx, umobj)
 }
