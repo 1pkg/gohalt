a4903c860e8e50342114d84368398cfbe5974dc4
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-07-02 19:28:37 +0200

    add sync runner

--- runners.go
+++ runners.go
@@ -6,17 +6,66 @@ import (
 	"sync"
 )
 
-type Runner struct {
+type Runner interface {
+	Run(context.Context, Runnable)
+	Result() error
+}
+
+type rsync struct {
 	thr    Throttler
-	wg     sync.WaitGroup
-	ctx    context.Context
 	err    error
 	report func(error)
 }
 
-func NewRunner(ctx context.Context, thr Throttler) (*Runner, context.Context) {
+func NewRunnerSync(ctx context.Context, thr Throttler) (*rsync, context.Context) {
 	ctx, cancel := context.WithCancel(ctx)
-	r := Runner{thr: thr, ctx: ctx}
+	r := rsync{thr: thr}
+	r.report = func(err error) {
+		if r.err != nil {
+			r.err = err
+			cancel()
+		}
+	}
+	return &r, ctx
+}
+
+func (r *rsync) Run(ctx context.Context, run Runnable) {
+	defer func() {
+		if err := r.thr.Release(ctx); err != nil {
+			r.report(fmt.Errorf("throttler error has happened %w", err))
+		}
+		return
+	}()
+	if err := r.thr.Acquire(ctx); err != nil {
+		r.report(fmt.Errorf("throttler error has happened %w", err))
+		return
+	}
+	select {
+	case <-ctx.Done():
+		r.report(fmt.Errorf("context error has happened %w", ctx.Err()))
+		return
+	default:
+	}
+	if err := run(ctx); err != nil {
+		r.report(fmt.Errorf("function error has happened %w", err))
+		return
+	}
+}
+
+func (r *rsync) Result() error {
+	return r.err
+}
+
+type rasync struct {
+	thr    Throttler
+	wg     sync.WaitGroup
+	err    error
+	report func(error)
+}
+
+func NewRunnerAsync(ctx context.Context, thr Throttler) (*rasync, context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	r := rasync{thr: thr}
 	var once sync.Once
 	r.report = func(err error) {
 		once.Do(func() {
@@ -27,26 +76,7 @@ func NewRunner(ctx context.Context, thr Throttler) (*Runner, context.Context) {
 	return &r, ctx
 }
 
-func (r *Runner) Go(run Runnable) {
-	r.GoWithContext(r.ctx, run)
-}
-
-func (r *Runner) GoWithParams(
-	run Runnable,
-	priority uint8,
-	key interface{},
-	data interface{},
-	marshaler Marshaler,
-) {
-	r.ctx = WithPriority(r.ctx, priority)
-	r.ctx = WithKey(r.ctx, key)
-	r.ctx = WithData(r.ctx, data)
-	r.ctx = WithTimestamp(r.ctx)
-	r.ctx = WithMarshaler(r.ctx, marshaler)
-	r.GoWithContext(r.ctx, run)
-}
-
-func (r *Runner) GoWithContext(ctx context.Context, run Runnable) {
+func (r *rasync) Run(ctx context.Context, run Runnable) {
 	r.wg.Add(1)
 	go func() {
 		defer func() {
@@ -73,8 +103,26 @@ func (r *Runner) GoWithContext(ctx context.Context, run Runnable) {
 	}()
 }
 
-func (r *Runner) Wait() error {
+func (r *rasync) Result() error {
 	r.wg.Wait()
 	r.report(nil)
 	return r.err
+}
+
+func RunWithParams(
+	r Runner,
+	run Runnable,
+	priority uint8,
+	key interface{},
+	data interface{},
+	marshaler Marshaler,
+) error {
+	ctx := context.Background()
+	ctx = WithPriority(ctx, priority)
+	ctx = WithKey(ctx, key)
+	ctx = WithData(ctx, data)
+	ctx = WithTimestamp(ctx)
+	ctx = WithMarshaler(ctx, marshaler)
+	r.Run(ctx, run)
+	return r.Result()
 }
