df3095688d340e764fe734fc03a377b36706218d
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-07-02 19:34:48 +0200

    runners refactoring

--- runners.go
+++ runners.go
@@ -7,46 +7,47 @@ import (
 )
 
 type Runner interface {
-	Run(context.Context, Runnable)
+	Run(Runnable)
 	Result() error
 }
 
 type rsync struct {
 	thr    Throttler
+	ctx    context.Context
 	err    error
 	report func(error)
 }
 
-func NewRunnerSync(ctx context.Context, thr Throttler) (*rsync, context.Context) {
+func NewRunnerSync(ctx context.Context, thr Throttler) *rsync {
 	ctx, cancel := context.WithCancel(ctx)
-	r := rsync{thr: thr}
+	r := rsync{thr: thr, ctx: ctx}
 	r.report = func(err error) {
 		if r.err != nil {
 			r.err = err
 			cancel()
 		}
 	}
-	return &r, ctx
+	return &r
 }
 
-func (r *rsync) Run(ctx context.Context, run Runnable) {
+func (r *rsync) Run(run Runnable) {
 	defer func() {
-		if err := r.thr.Release(ctx); err != nil {
+		if err := r.thr.Release(r.ctx); err != nil {
 			r.report(fmt.Errorf("throttler error has happened %w", err))
 		}
 		return
 	}()
-	if err := r.thr.Acquire(ctx); err != nil {
+	if err := r.thr.Acquire(r.ctx); err != nil {
 		r.report(fmt.Errorf("throttler error has happened %w", err))
 		return
 	}
 	select {
-	case <-ctx.Done():
-		r.report(fmt.Errorf("context error has happened %w", ctx.Err()))
+	case <-r.ctx.Done():
+		r.report(fmt.Errorf("context error has happened %w", r.ctx.Err()))
 		return
 	default:
 	}
-	if err := run(ctx); err != nil {
+	if err := run(r.ctx); err != nil {
 		r.report(fmt.Errorf("function error has happened %w", err))
 		return
 	}
@@ -58,14 +59,15 @@ func (r *rsync) Result() error {
 
 type rasync struct {
 	thr    Throttler
+	ctx    context.Context
 	wg     sync.WaitGroup
 	err    error
 	report func(error)
 }
 
-func NewRunnerAsync(ctx context.Context, thr Throttler) (*rasync, context.Context) {
+func NewRunnerAsync(ctx context.Context, thr Throttler) *rasync {
 	ctx, cancel := context.WithCancel(ctx)
-	r := rasync{thr: thr}
+	r := rasync{thr: thr, ctx: ctx}
 	var once sync.Once
 	r.report = func(err error) {
 		once.Do(func() {
@@ -73,30 +75,30 @@ func NewRunnerAsync(ctx context.Context, thr Throttler) (*rasync, context.Contex
 			cancel()
 		})
 	}
-	return &r, ctx
+	return &r
 }
 
-func (r *rasync) Run(ctx context.Context, run Runnable) {
+func (r *rasync) Run(run Runnable) {
 	r.wg.Add(1)
 	go func() {
 		defer func() {
-			if err := r.thr.Release(ctx); err != nil {
+			if err := r.thr.Release(r.ctx); err != nil {
 				r.report(fmt.Errorf("throttler error has happened %w", err))
 			}
 			r.wg.Done()
 			return
 		}()
-		if err := r.thr.Acquire(ctx); err != nil {
+		if err := r.thr.Acquire(r.ctx); err != nil {
 			r.report(fmt.Errorf("throttler error has happened %w", err))
 			return
 		}
 		select {
-		case <-ctx.Done():
-			r.report(fmt.Errorf("context error has happened %w", ctx.Err()))
+		case <-r.ctx.Done():
+			r.report(fmt.Errorf("context error has happened %w", r.ctx.Err()))
 			return
 		default:
 		}
-		if err := run(ctx); err != nil {
+		if err := run(r.ctx); err != nil {
 			r.report(fmt.Errorf("function error has happened %w", err))
 			return
 		}
@@ -107,22 +109,4 @@ func (r *rasync) Result() error {
 	r.wg.Wait()
 	r.report(nil)
 	return r.err
-}
-
-func RunWithParams(
-	r Runner,
-	run Runnable,
-	priority uint8,
-	key interface{},
-	data interface{},
-	marshaler Marshaler,
-) error {
-	ctx := context.Background()
-	ctx = WithPriority(ctx, priority)
-	ctx = WithKey(ctx, key)
-	ctx = WithData(ctx, data)
-	ctx = WithTimestamp(ctx)
-	ctx = WithMarshaler(ctx, marshaler)
-	r.Run(ctx, run)
-	return r.Result()
 }
